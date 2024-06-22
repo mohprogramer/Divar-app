7b241c0cc0da797e36a0e03a3fc7e55676a307ca
Mohammad Davodi <mohammaddavodi96@gmail.com>
2024-06-23 00:41:40 +0330

    sendotp page

--- Front-end/src/pages/AuthPage.jsx
+++ Front-end/src/pages/AuthPage.jsx
@@ -5,12 +5,12 @@ import SendOtpForm from "../components/templates/SendOtpForm";
 function AuthPage() {
 
     const [step, setStep] = useState(1)
-    const [mobile, setMobile] = useState(1)
-    const [code, setCode] = useState(1)
+    const [mobile, setMobile] = useState("")
+    const [code, setCode] = useState("")
 
     return (
         <div>
-            {step == 1 && <SendOtpForm /> }
+            {step == 1 && <SendOtpForm setStep={setStep} mobile={mobile} setMobile={setMobile} /> }
             {step == 2 && <CheckOtpForm /> }
         </div>
     );
