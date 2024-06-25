9d2496b88ff030e9c885309b63eee894520272e3
Mohammad Davodi <mohammaddavodi96@gmail.com>
2024-06-26 01:01:44 +0330

    refreshToken

--- Front-end/src/configs/api.js
+++ Front-end/src/configs/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { getCookie } from "../utils/cookie";
+import { getCookie, setCookie } from "../utils/cookie";
+import { getNewToken } from "../services/tokens";
 
 
 const api = axios.create({
@@ -15,6 +16,25 @@ api.interceptors.request.use((request) => {
         request.headers["Authorization"] = `bearer ${accessToken}`
     }
     return request
+}, (error) =>{
+    return Promise.reject(error)
+})
+
+api.interceptors.response.use((response) => {
+    return response;
+}, async (error) => {
+    console.log("hallo")
+  const originalRequest = error.config;
+  if(error.response.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    const res = await getNewToken()
+    console.log(res)
+    if(!res?.response) return;
+    console.log(res)
+    
+    setCookie(res.response.data)
+    return api(originalRequest)
+  }
 })
 
 export default api
